ec3df224e0f79064fae537887ad9e5f8ed870f76
shark8me <kiran.karkera@gmail.com>
2018-09-14 18:29:27 +0800

    resolves #2

--- src/koi/zeppelin.clj
+++ src/koi/zeppelin.clj
@@ -2,12 +2,22 @@
   (:require [clojure.spec.alpha :as s]
             [clojure.spec.test.alpha :as stest]
             [koi.spec :as ps ]
+            [clojure.data.json :as json]
+            [org.httpkit.client :as ht]
             [promenade.core :as prom])
   (:import [java.util UUID]))
 
 (defn invoke-job
   "invokes a job"
   ([{:keys [::ps/accountId ::ps/algorithmAssetId] :as job-args}]
+   (let [note-uri "https://github.com/hortonworks-gallery/zeppelin-notebooks/raw/master/2C23PDD5H/note.json"
+         k (ht/get note-uri)
+     resp @(ht/request {:url "http://localhost:8080/api/notebook/import"
+                            :body (-> @k :body)
+                            :method :post})
+         resp @(ht/request {:url (str "http://localhost:8080/api/notebook/job/"
+                                      (-> resp :body json/read-str (get "body")))
+                            :method :post})])
    (println " invoked job with " job-args)))
 
 (s/fdef invoke-job :args (s/cat :job-args ::ps/invoke-job-keys))
