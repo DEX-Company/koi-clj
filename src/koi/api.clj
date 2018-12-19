1f765578e0293206b8b7d664b1f649027b212661
shark8me <kiran.karkera@gmail.com>
2018-12-19 09:34:44 +0530

    resolves #3

--- src/koi/api.clj
+++ src/koi/api.clj
@@ -7,6 +7,7 @@
                                        undocumented
                                        GET PUT POST DELETE]]
    [ring.middleware.cors :refer [wrap-cors]]
+   [org.httpkit.client :as http]
    [compojure.api.coercion.schema :as cos]
    [compojure.route :as route]
    [schema.core :as s]
@@ -18,6 +19,7 @@
    [muuntaja.core :as muuntaja]
    [koi.invokable :refer [invoke get-status get-result get-proof]]
    [muuntaja.format.json :as json-format]
+   [cheshire.core :as che :refer [generate-string parse-stream]]
    [koi.spec :as ps]
    [org.httpkit.client :as ht]
    [clojure.data.json :as json])
@@ -50,11 +52,26 @@
   {:status s/Str
    (s/optional-key :errorCode) s/Num})
 
-(s/defschema JobResultResp
-  {:result s/Any})
+(s/defschema OcnAsset
+  {(s/optional-key :assetid) s/Str
+   :asseturl s/Str
+   :argname s/Str
+   (s/optional-key :serviceagreementid) s/Str
+   (s/optional-key :assetmetadataurl) s/Str})
 
-(s/defschema InvokeJobResp
-  {:jobid s/Str})
+(s/defschema JobResultResp
+  {:result [OcnAsset]
+   (s/optional-key :errorcode) s/Str})
+
+(s/defschema OcnInputs
+  {:inputs [OcnAsset]
+   (s/optional-key :consumerid) s/Str
+   (s/optional-key :invokeserviceagreementid) s/Str})
+
+(s/defschema InvokeJobReq
+  {:oceaninputs OcnInputs
+   (s/optional-key :configuration) s/Any})
+
 
 (s/defschema GetSvcResp
   {:services s/Any})
@@ -65,6 +82,15 @@
    :method s/Str
    ;;base64 encoded string
    :payload s/Str})
+
+
+;;
+
+
+
+(s/defschema InvokeJobReq
+  {:oceaninputs OcnInputs
+   (s/optional-key :configuration) s/Any})
 
 (s/defschema InvokeSvcResp
   {:response s/Any})
@@ -85,6 +111,73 @@
 
 (def services (atom {}))
 (def status (atom {}))
+(def dest-file "/tmp/numlist")
+(defn get-asset  
+  "get the asset at URL"
+  [ast-url]
+  (let [k @(http/request {:url ast-url :method :get})
+        op (-> k :body)]
+    (clojure.java.io/copy op
+                          (clojure.java.io/file dest-file))
+    dest-file))
+
+(defn register-asset
+  [metadata]
+  (let [k @(http/request
+            {:url "http://13.67.33.157:8080/api/v1/meta/data"
+             :method :post
+             :body (generate-string metadata)
+             :basic-auth ["test" "foobar"]})]
+    k))
+
+(def jobs-db (atom {}))
+(defn invoke-job
+  [{:keys [oceaninputs configuration] :as m}]
+  (let [asturl (-> oceaninputs :inputs first :asseturl)
+        dstfile (get-asset asturl)
+        result 
+        (apply + (mapv #(Integer/parseInt %)
+                       (-> (slurp dstfile)
+                           (.split "\n") seq)))
+        resp (register-asset {:name "addition result "
+                                      :description "description "
+                                      :type "dataset"
+                                      ;;add prov metadata here
+                                      })]
+    (println " result of invoke is " result)
+    (spit "/tmp/result" result)
+    (-> resp :body io/reader parse-stream)))
+
+(defn insert-job
+  [ireq]
+  (let [uid (.toString (UUID/randomUUID))
+        _ (swap! jobs-db #(update-in % [:status uid] (fn [_]:registered)))
+        ;;move this to a separate thread
+        resp (invoke-job ireq)]
+    (swap! jobs-db #(-> %
+                        (update-in [:result uid] (fn[_]resp))
+                        (update-in [:status uid] (fn[_]:completed))))
+    uid))
+
+;;an atom to hold the service registration
+(def service-id (atom nil))
+
+(defn register-metadata
+  "register the service"
+  []
+  (if @service-id @service-id 
+      (let [k (register-asset {:name "datasetname "
+                               :description "description "
+                               :links [{:name ""
+                                        :type "algorithm"
+                                        :url "abcd"}]
+                               :type "service"})
+            ast-id (-> k :body io/reader parse-stream)]
+        (reset! service-id ast-id)
+        ast-id)))
+(s/defschema InvokeJobResp
+  {:jobid s/Str})
+
 (def app
   (->
    (api
@@ -99,68 +192,50 @@
              :consumes ["application/json"]
              }}}
 
-    (context "/"
+    (context "/register"
              []
-             :tags ["Invoke API "]
+             :tags ["Register the service "]
+             (POST "/" []
+                   :return s/Str
+                   :summary "Register"
+                   (ok (register-metadata))))
 
-             (POST "/register" []
-                   :return RegSvcResp
-                   :body [ireq RegSvcReq]
-                   :summary "register a service"
-                   (let [{:keys [:iri] :as m } ireq
-                         _ (println " inputs  " m)
-                         uuid (.toString (UUID/randomUUID))
-                         pathdoc (parse-swagger iri)
-                         res (swap! services assoc uuid pathdoc)]
-                     (ok {:serviceid uuid})))
+    (context "/jobs"
+             []
+             :tags ["Invoke addition API "]
 
-             (GET "/services" []
-               :return GetSvcResp 
-               :summary "register a service"
-               (let [res @services]
-                 (ok {:services res})))
+             (POST "/" []
+                   :return InvokeJobResp
+                   :body [ireq InvokeJobReq]
+                   :summary "Invoke a addition job"
+                   (ok {:jobid (insert-job ireq)}))
 
-             (POST "/invoke" []
-                  :return InvokeSvcResp
-                  :body [ireq InvokeSvcReq]
-                  :summary "Returns result of job"
-                  (let [{:keys [serviceid path method payload] :as m} ireq
-                        svcuri (get @services serviceid)
-                        valid-paths (get-in svcuri [:paths path method])
-                        payload (json/read-str (String. (decode payload)))
-                        svcuri2 (str svcuri path)
-                        _ (println " svcuri path "  svcuri " - " svcuri2 " - val " valid-paths)]
-                    (if (nil? valid-paths)
-                      (bad-request "invalid path " m )
-                      (let [host (get svcuri :uri)
-                            host2 (.substring host 0 (dec (.length host)))
-                            apiurl (str host2 path)
-                            _ (println " api url " apiurl)
-                            resp @(ht/request {:url apiurl 
-                                               :method (keyword method)
-                                               :headers {"Content-Type" "application/json"}
-                                               :body (json/write-str payload)})]
-                        (println " invoke ---- " serviceid " - " svcuri " - " path " - " method  " - " payload
-                                 " response " resp)
-                        (ok {:response (:body resp)})))))
-
-             (POST "/invokeOcn" []
-                   :return InvokeSvcResp
-                   :body [ireq InvokeSvcReq]
-                   :summary "Returns result of job"
-                   (let [{:keys [serviceid path method payload] :as m} ireq
-                         svcuri (get @services serviceid)
-                         _ (println serviceid " - " svcuri " - " path " - " method  " - " (json/read-str payload))
-                         resp @(ht/request {:url (str svcuri path)
-                                            :method (keyword method)
-                                            :body payload})]
-                     (ok {:response resp})))
-
-             #_(GET "/status/:jobId" []
+             (GET "/status/:jobId" []
                   :return JobStatusResp
                   :path-params [jobId :- s/Str]
                   :summary "Returns status of job"
-                  (ok {:status (name (get-status adsvc jobId))}))))
+                  (ok {:status
+                       (let [status (get-in @jobs-db [:status jobId])]
+                         (if status (name status)
+                             "invalid jobid"))}))
+
+             (GET "/result/:jobId" []
+                  :return JobResultResp
+                  :path-params [jobId :- s/Str]
+                  :query-params [consumerId :- s/Str]
+                  :summary "Returns result of job"
+                  (ok (let [i (get-in @jobs-db
+                                      [:result jobId])]
+                        (println " result is "  i)
+                        (if i {:result [{:assetid i
+                                         :asseturl "url"
+                                         :argname "a"}]}
+                            ;;poor error handling
+                            {:result [{:assetid "invalid" 
+                                       :asseturl "url"
+                                       :argname "a"}]
+                             :errorcode "400"}))))))
+
    (wrap-cors :access-control-allow-origin [#"http://localhost:3449"]
               :access-control-allow-methods [:get :put :post :delete])))
 
