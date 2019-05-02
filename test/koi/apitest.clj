1a17bec384086f8ad2b275cda1b8eae6fbf9ac5e
shark8me <kiran.karkera@gmail.com>
2019-05-02 16:19:09 +0800

    removed body content for request to get job details

--- test/koi/apitest.clj
+++ test/koi/apitest.clj
@@ -2,6 +2,7 @@
   (:require  [clojure.test :as t :refer [deftest is testing]]
              [cheshire.core :as cheshire]
              [ring.util.http-response :as http-response :refer [ok header created unprocessable-entity
+                                                                internal-server-error
                                                                 bad-request]]
              [ring.mock.request :as mock]
              [koi.api :as api :refer [app]])
@@ -32,6 +33,21 @@
                             (mock/content-type "application/json")
                             ;;hashing needs to-hash as an argument
                             (mock/body (cheshire/generate-string {:abc "def"}))))]
-      (is (= (:status response) (:status (bad-request)))))))
-
-
+      (is (= (:status response) (:status (bad-request))))))
+  (testing "Test failing operation"
+    (let [response (app (-> (mock/request :post "/invoke/fail")
+                            (mock/content-type "application/json")
+                            (mock/body (cheshire/generate-string {:dummy "def"}))))]
+      (is (= (:status response) (:status (internal-server-error))))))
+  (testing "Test async failing operation"
+    (let [response (app (-> (mock/request :post "/invokeasync/fail")
+                            (mock/content-type "application/json")
+                            (mock/body (cheshire/generate-string {:dummy "def"}))))
+          jobid     (:jobid (parse-body (:body response)))
+          jobres (app (-> (mock/request :get (str "/jobs/" jobid))
+                          (mock/content-type "application/json")))
+          job-body (parse-body (:body jobres))
+          ]
+      (is (= (:status response) (:status (created))))
+      (is (= (:status jobres) (:status (ok))))
+      (is (every? #{:status :errorcode :description} (keys job-body))))))
