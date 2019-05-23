fcf6a3e5e2cad88fcf64560131c45cda9125ec6d
shark8me <kiran.karkera@gmail.com>
2019-05-23 20:00:27 +0800

    added echo to docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-SURFER_URL="http://localhost:8080"
 CURL_CMD="curl -w httpcode=%{http_code}"
 
 # See retry options https://stackoverflow.com/a/42873372/845843
@@ -9,7 +8,7 @@ CURL_MAX_CONNECTION_TIMEOUT="--retry-max-time 40 --retry-connrefused --retry 10 
 # perform curl operation
 CURL_RETURN_CODE=0
 CURL_OUTPUT=`${CURL_CMD} ${CURL_MAX_CONNECTION_TIMEOUT} ${SURFER_URL} 2> /dev/null` || CURL_RETURN_CODE=$?
-echo "Waiting for Surfer to be up"
+echo "Waiting for Surfer to be up at $SURFER_URL"
 if [ ${CURL_RETURN_CODE} -ne 0 ]; then
     echo "Curl connection failed with return code - ${CURL_RETURN_CODE}"
 else
