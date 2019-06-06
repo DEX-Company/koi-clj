7f919ed36c5cbe4097fb14be7d8248ad0f00ed84
shark8me <kiran.karkera@gmail.com>
2019-06-06 18:37:17 +0800

    change port to 8191, enable changing port from dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-./scripts/wait_for_surfer.sh ${SURFER_URL}
+./scripts/wait_for_surfer.sh ${AGENT_URL}
 
 if [ $? -eq 0 ]; then
-    port=8191 java -jar target/koi-clj-0.1.0-SNAPSHOT-standalone.jar
+    port=${KOI_PORT} java -jar target/koi-clj-0.1.3-standalone.jar
     tail -f /dev/null
 fi
