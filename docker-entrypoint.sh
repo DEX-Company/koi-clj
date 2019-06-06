8b2c97a15174bf59425a7a6ce2dc01252b93db72
shark8me <kiran.karkera@gmail.com>
2019-06-06 18:12:12 +0800

    changed port to 8191

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,6 @@
 ./scripts/wait_for_surfer.sh ${SURFER_URL}
 
 if [ $? -eq 0 ]; then
-    java -jar target/koi-clj-0.1.0-SNAPSHOT-standalone.jar
+    port=8191 java -jar target/koi-clj-0.1.0-SNAPSHOT-standalone.jar
     tail -f /dev/null
 fi
