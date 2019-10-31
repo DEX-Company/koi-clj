e4765a704441e4c9ccbf8f391ac1ea935c943e79
shark8me <kiran.karkera@gmail.com>
2019-11-01 07:38:30 +0800

    changed names of inputs

--- src/koi/examples/concat_datasets.clj
+++ src/koi/examples/concat_datasets.clj
@@ -1,9 +1,13 @@
 (ns koi.examples.concat-datasets
   (:require [starfish.core :as s]
             [clojure.data.json :as json]
+            [cheshire.core :as cjson]
             [clojure.java.io :as io])
+  (:import (java.io PrintWriter PushbackReader StringWriter
+                    Writer StringReader EOFException))
   (:import [sg.dex.starfish.impl.squid SquidResolverImpl SquidAgent]
-           [sg.dex.starfish.util DID]))
+           [sg.dex.starfish.util DID]
+           [java.io StringWriter]))
 
 (defn merge-maps
   "merge maps, which is wrapped by a vector"
@@ -12,12 +16,18 @@
     [(apply merge maps)]))
 
 (defn concat-dataset
-  [{:keys [dataset1 dataset2 dataset3] :as assets}]
+  [{:keys [engine-logbook engine-removal-notification engine-shop-visit-report] :as assets}]
   (println " type of assets " assets)
   (let [slurp-fn #(->> % s/content s/to-string json/read-str)
-        merged-map (merge-maps [dataset1 dataset2 dataset3] slurp-fn)
-        resp-dset (s/asset (s/memory-asset (json/write-str merged-map)))
-        res {:concatenated-dataset resp-dset}
+        merged-map (merge-maps [engine-logbook engine-removal-notification engine-shop-visit-report] slurp-fn)
+        resp-dset (s/asset (s/memory-asset
+                            {
+                             :name "Engine Maintenance Dashboard"
+                             :author "Data Processor inc"
+                             }
+                            (cjson/generate-string
+                             merged-map {:pretty true})))
+        res {:engine-maintenance-dashboard resp-dset}
         _ (println " generated concat dataset, now registering with blockchain ")
         squid-agent (SquidAgent/create (SquidResolverImpl.) (DID/createRandom))
         r2 (s/register squid-agent resp-dset)
@@ -50,7 +60,7 @@
   (spit "/tmp/f.json"
         (json/write-str (merge-maps inp-datasets (fn[i] (-> i io/resource slurp json/read-str)))))
 
-  (-> (concat-dataset (zipmap (mapv #(keyword (str "dataset" %)) (range 1 4)) t2))
+  (-> (concat-dataset (zipmap [:engine-logbook :engine-removal-notification :engine-shop-visit-report] t2))
       s/content s/to-string json/read-str)
   )
 
